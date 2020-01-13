7bd6afb7a347f34620439c5b858de3c0cae2e43a
krishnageddam <krishnavardhan.geddam@gmail.com>
2020-01-13 18:32:28 +0530

    push client vue code

---
+++ dist/precache-manifest.77405a7e8dbac3151a8624b8b007e12f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0760e19f775c25f964ed",
+    "url": "/css/app.css"
+  },
+  {
+    "revision": "da456e7a401bb2fda5fc",
+    "url": "/css/chunk-app.css"
+  },
+  {
+    "revision": "d544825ca3d4f7e185989f367e2f6b25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0760e19f775c25f964ed",
+    "url": "/js/app.js"
+  },
+  {
+    "revision": "da456e7a401bb2fda5fc",
+    "url": "/js/chunk-app.js"
+  },
+  {
+    "revision": "a3b77eeb6a16462cb025d0ed4ce244a7",
+    "url": "/manifest.json"
+  }
+]);
